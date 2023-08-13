bb62f405835faad8a6722f36eee083c8fda98687
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-13 20:27:48 +0200

    Added table for latest samples

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -10,7 +10,8 @@ import { Field, Form, SubmitHandler, createForm } from "@modular-forms/solid";
 import { SamplesFileUploadForm } from "../types_definition/forms";
 import { SamplesChart } from "../components/SamplesChart";
 
-import { fetchAverageDaySamples } from "../utils/fetching";
+import { fetchAverageDaySamples, fetchLatestSamples } from "../utils/fetching";
+import DataTable from "../components/DataTable";
 
 const Summary: Component = (props) => {
     const [drawerVisible, setDrawerVisible] = createSignal(false);
@@ -19,6 +20,11 @@ const Summary: Component = (props) => {
     <Drawer visible={drawerVisible()} />
     <div class={styles.mainContent}>
         <SamplesChart fetching={fetchAverageDaySamples} />
+        <DataTable
+          fetching={[fetchLatestSamples]}
+          maxItemDisplayed={3}
+          tableTitle="Latest samples"
+        />
     </div>
     <button class={styles.drawerToggler} onClick={(ev) => setDrawerVisible(v => !v)}>
         <span></span>
