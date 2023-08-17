c930f07513d053eddd4a14d145ddb2ee667c56c8
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-17 20:05:13 +0200

    Moved styles in variables

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,4 +1,4 @@
-import { Component, Match, Show, Switch, createResource, createSignal } from "solid-js";
+import { Component, Match, Switch, createResource, createSignal, JSX } from "solid-js";
 import { DragAndDropProps } from "../types_definition/props";
 import Loader from "./Loader";
 
@@ -11,7 +11,6 @@ type FileInputEvent = InputEvent & {
   currentTarget: HTMLInputElement;
   target: HTMLInputElement;
 }
-
 
 const DragAndDrop: Component<DragAndDropProps> = (props) => {
   const [droppedFile, setDroppedFile] = createSignal<File|null>(null);
@@ -35,46 +34,11 @@ const DragAndDrop: Component<DragAndDropProps> = (props) => {
   }
   return <section>
     <form style={{display: "flex", "flex-direction": "column", "align-items": "center"}}>
-        <div
-          style={{
-            position: "relative",
-            border: "3px dashed #5CD8F3",
-            "border-radius": "14px",
-            width: "600px",
-            "min-height": "200px",
-            "display": "flex",
-            "justify-content": "center",
-            "align-items": "center"
-          }}>
-          <input
-            style={{
-              opacity: "0",
-              "position": "absolute",
-              "top": "0",
-              "left": 0,
-              "width": "100%",
-              "height":
-              "100%"
-            }}
-            type="file"
-            onInput={(ev) => handleFileInput(ev)} />
-          
+        <div style={dropArea}>
+          <input style={fileInput} type="file" onInput={(ev) => handleFileInput(ev)} />
           <p style={{color: "#5CD8F3"}}>{droppedFile() ? droppedFile()!.name : props.sendButtonText}</p>
         </div>
-        <button
-          style={{
-            display: "block",
-            "background-color": "#5CD8F3",
-            color: "white",
-            "font-size": "20px",
-            padding: "10px",
-            "margin-top": "25px",
-            width: "200px",
-            border: "none",
-            "border-radius": "50px"
-          }}
-          type="submit" onClick={(ev) => handleSubmit(ev)}
-        >Upload</button>
+        <button style={submitButton} type="submit" onClick={(ev) => handleSubmit(ev)}>Upload</button>
         <Switch>
           <Match when={uploadResponse()}>
               <p>{uploadResponse()!.message}</p>
@@ -86,5 +50,37 @@ const DragAndDrop: Component<DragAndDropProps> = (props) => {
     </form>
   </section>
 };
+
+const dropArea: JSX.CSSProperties = {
+  position: "relative",
+  border: "3px dashed #5CD8F3",
+  "border-radius": "14px",
+  width: "600px",
+  "min-height": "200px",
+  "display": "flex",
+  "justify-content": "center",
+  "align-items": "center"
+}
+
+const fileInput: JSX.CSSProperties = {
+  opacity: "0",
+  "position": "absolute",
+  "top": "0",
+  "left": 0,
+  "width": "100%",
+  "height": "100%"
+}
+
+const submitButton: JSX.CSSProperties = {
+  display: "block",
+  "background-color": "#5CD8F3",
+  color: "white",
+  "font-size": "20px",
+  padding: "10px",
+  "margin-top": "25px",
+  width: "200px",
+  border: "none",
+  "border-radius": "50px"
+}
 
 export default DragAndDrop;
